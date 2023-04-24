edb0d10e1f58443ec7bb0512887285746ee82e58
Yelong Hartl-He <Yelong.he@hotmail.com>
2023-04-24 20:07:18 +0200

    added plotly with fake data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 # Set the title for the program
 st.title("Weather Forecast Prediction")
@@ -7,13 +8,20 @@ st.title("Weather Forecast Prediction")
 location = st.text_input("Place: ")
 
 # Forecast days slider
-days = st.slider("Forecast in days", min_value=1, max_value=5,help="Select the number of desired days of weather forecasting")
+days = st.slider("Forecast in days", min_value=1, max_value=5,
+                 help="Select the number of desired days of weather forecasting")
 
+# A dropdrown so user can change either temperature or sky
 options = st.selectbox("Select data to view",
-                                  ("Temperature", "Sky"))
+                       ("Temperature", "Sky"))
 
 if days <= 1:
     st.subheader(f"{options} for the next {days} day in {location}")
 else:
     st.subheader(f"{options} for the next {days} days in {location}")
 
+date = ["2222-24-10", "2022-26-10", "2022-27-10", "2022-28-10", "2022-29-10"]
+temperatures = [10, 8.5, 5, 7.6, 6.7]
+temperatures = [days * i for i in temperatures]
+figure = px.line(x=date, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
