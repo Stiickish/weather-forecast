4e542d89b4fa82522f3444b9b8fd2fb50cb84cdb
Yelong Hartl-He <Yelong.he@hotmail.com>
2023-04-25 12:55:17 +0200

    clean ups

--- main.py
+++ main.py
@@ -12,26 +12,35 @@ location = st.text_input("Place: ")
 days = st.slider("Forecast in days", min_value=1, max_value=5,
                  help="Select the number of desired days of weather forecasting")
 
-# A dropdrown so user can change either temperature or sky
+# A dropdown so user can change either temperature or sky
 options = st.selectbox("Select data to view",
                        ("Temperature", "Sky"))
 
+# Condition check. Shows either day or days
 if days <= 1:
     st.subheader(f"{options} for the next {days} day in {location}")
 else:
     st.subheader(f"{options} for the next {days} days in {location}")
 
+# Checks if there is a location.
 if location:
-    # Get the data from backend
     try:
+        # Get the data from backend, location and days
         filtered_data = get_data(location, days)
 
+        # Checks if user input is temperature
+        # Temperature data is extracted and filtered
+        # Data is plotted
         if options == "Temperature":
             temperatures = [dict["main"]["temp"] for dict in filtered_data]
             dates = [dict["dt_txt"] for dict in filtered_data]
             figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
             st.plotly_chart(figure)
 
+        # Checks if the user input is sky
+        # Sky data is extracted and filtered
+        # Using list comprehensions to map through the list of weather data
+        # Using second list comprehensions to create image path corresponding to condition
         if options == "Sky":
             images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png",
                       "Snow": "images/snow.png"}
